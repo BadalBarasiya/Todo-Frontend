dac14a58e0ecd7c60fe75ad8fbb5b483333115e6
BadalBarasiya <badalbarasiya888@gmail.com>
2026-04-26 16:37:54 +0530

    [26-03]: "fix env name"

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:9000/api";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:9000/api";
 
 // helper function
 export const apiRequest = async (endpoint, method = "GET", body = null) => {
